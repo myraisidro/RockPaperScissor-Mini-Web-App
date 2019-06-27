53eeced58da7e1f21ee1315704d984ce35a9ea22
myraisidro <myraisidro004@gmail.com>
2019-06-27 17:06:39 +0800

    Ne update

--- app.js
+++ app.js
@@ -1,74 +1,133 @@
+// Declaring default values for scoreboard
+let UserScore = 0,
+    ComputerScore = 0,
+    playerChoice = 0,
+    startingCoins = 3,
+    computerChoice = 0,
+    take = '';
+
 // Variables for document ID's
 const rock = document.getElementById('rock'),
     paper = document.getElementById('paper'),
     scissor = document.getElementById('scissor'),
     userScore = document.getElementById('user-score'),
     computerScore = document.getElementById('computer-score'),
+    takePick = document.querySelector('.strong'),
+    waitPick = document.querySelector('.pickMsg'),
     coin = document.getElementById('coins');
 
-// Declaring default values for scoreboard
-let UserScore = 0,
-    ComputerScore = 0,
-    playerChoice = 0,
-    startingCoins = 3,
-    computerChoice = 0;
 
 // Event listeners uses to add an event to our buttons
 rock.addEventListener('click' , RockChoice); //function for rock
 paper.addEventListener('click' , paperChoice); //function for paper
 scissor.addEventListener('click' , scissorChoice); //function for paper
+document.addEventListener('DOMContentLoaded' , displayScoreboard);  //function to loadfrom local
+
 
 // Display default values declared to scoreboard
 userScore.textContent = UserScore;
 computerScore.textContent = ComputerScore;
 coin.textContent = startingCoins;
 
-// Functions for Rock = 1 ; 
-function RockChoice(e) {
-    playerChoice = 1; //binigyan lang ng value si Rock
-    computerChoice = Math.floor(Math.random() * 3 + 1); //para mag random si computer then para mabigyan ng value si computer choicce from 1-3
+// Set imgs to variable
+const uRock = 'images/rock-left.png';
+const uPaper = 'images/paper-left.png';
+const uScissor = 'images/scissor-left.png';
+const cRock = 'images/rock-right.png';
+const cPaper = 'images/paper-right.png';
+const cScissor = 'images/scissor-right.png';
 
-        // Create Element for image
-        const img = document.createElement('img'); //create img element 
-        img.setAttribute('src', 'images/rock-left.png'); //add src attribute to img tag
-        img.setAttribute('height', '300'); // add height attribute to img tag
-        document.querySelector('.userPick').appendChild(img);
 
-        rock.classList.add('disabled');
+// Create Local Storage
+function createLocalStorage(){ 
+    scoreBoard = {
+        UserScore : UserScore,
+        ComputerScore : ComputerScore,
+        startingCoins : startingCoins
+            }
+    let Game;
+        if(sessionStorage.getItem('Games') === null){
+        Game = [];
+    
+    } else {
+        Game = JSON.parse(sessionStorage.getItem('Games'));
+    } 
+    
+    Game.push(scoreBoard);
+    localStorage.setItem('Games',JSON.stringify(Game));
+    }
+    
+    // get from local Storage
+    function displayScoreboard(){
+        let game;
+    if(localStorage.getItem('Games') === null){
+        game = [];
+    
+    } else {
+      game = JSON.parse(localStorage.getItem('Games'));
+    } 
+    game.forEach(function(games){
+        computerScore.textContent = games.ComputerScore;
+        userScore.textContent = games.UserScore;
+        coin.textContent = games.startingCoins;
+    
+    });
+    }
+    
 
+// Function UI
+function uGesture(choice){
+    this.choice = choice;
+    const userPick = document.querySelector('.userPick');
+    userPick.innerHTML = `<img src="${this.choice}" height="300px">`;
+}
+function cGesture(choice){
+    this.choice = choice;
+    const userPick = document.querySelector('.compPick');
+    userPick.innerHTML = `<img src="${this.choice}" height="300px">`;
+}
+
+function appendMsg(){
         // Replace text on strong tag
         const strong = document.createTextNode('You made a pick!').textContent;
-        document.querySelector('.strong').innerHTML = strong;
+        document.querySelector('.headsUp').innerHTML = strong;
 
         // Replace text on takePick
         const waitPick = document.createTextNode('Wait for the opponent').textContent;
         document.querySelector('.pickMsg').style.fontSize = '40px';
         document.querySelector('.pickMsg').innerHTML = waitPick;
+}
 
+function Draw(){
+document.querySelector('.pickMsg').textContent = 'Draw'
+}
+
+
+// Functions for Rock = 1 ; 
+function RockChoice() {
+    playerChoice = 1; //binigyan lang ng value si Rock
+    computerChoice = Math.floor(Math.random() * 3 + 1); //para mag random si computer then para mabigyan ng value si computer choicce from 1-3
+
+    // uGestures Append
+    uGesture(uRock);
+    appendMsg();
 
     if(playerChoice === computerChoice) { //Draw no score would be added
-        console.log(`player:rock computer:rock`);
-        console.log('draw');
-        const img = document.createElement('img'); //create img element 
-        img.setAttribute('src', 'images/rock-right.png'); //add src attribute to img tag
-        img.setAttribute('height', '300'); // add height attribute to img tag
-        document.querySelector('.compPick').appendChild(img);
-
-        drawAlert();
-
-
+        cGesture(cRock);
+        Draw();
     } 
+
     else if (playerChoice == 1 && computerChoice == 2){ //PLAYER LOOSE! Player: Rock && Computer: Paper
-        console.log(`player:rock computer:paper`);
+        cGesture(cPaper);
         startingCoins--;   // deduct reward from user
         ComputerScore++; //add score to computer
-        console.log('You Loose');
+        
     } 
     else { //PLAYER WINS! Player: Rock && Computer: Scissor
-        console.log(`player:rock computer:scissor`); 
+        cGesture(cScissor);
         UserScore++; // add score to user
         startingCoins++ //add reward to user
-        console.log('You Win');
+        
     }
 
     userScore.textContent = UserScore; //display value of UserScore to the element tag na dala ni userScore
@@ -77,51 +136,41 @@ function RockChoice(e) {
 
     // kapag naubos na yung reward ni user "Game Over" na
     if (startingCoins === 0){
-        alert('Game Over');
         UserScore = 0; //declare the value to 0 (reset)
         ComputerScore = 0; //declare value to 0 (reset)
         userScore.textContent = UserScore;  
         computerScore.textContent = ComputerScore;
     }
+    createLocalStorage();
 }
 
 // Function for Paper = 2;
 function paperChoice(){
 
-    // Create Element for image
-    const img = document.createElement('img'); //create img element 
-    img.setAttribute('src', 'images/paper-left.png'); //add src attribute to img tag
-    img.setAttribute('height', '300'); // add height attribute to img tag
-    document.querySelector('.userPick').appendChild(img);
-
-    // Replace text on strong tag
-    const strong = document.createTextNode('You made a pick!').textContent;
-    document.querySelector('.strong').innerHTML = strong;
-
-    // Replace text on takePick
-    const waitPick = document.createTextNode('Wait for the opponent').textContent;
-    document.querySelector('.pickMsg').style.fontSize = '40px';
-    document.querySelector('.pickMsg').innerHTML = waitPick;
+    // uGestures Append
+    uGesture(uPaper);
+    appendMsg();
 
     playerChoice = 2;
     computerChoice = Math.floor(Math.random() * 3 + 1);
     
     if(playerChoice === computerChoice) {
-        console.log(`player:paper computer:paper`);
-        console.log('draw');
+        cGesture(cPaper);
+
+            
     }
     // computer choose rock / player choose paper
      else if (playerChoice == 2 && computerChoice ==1) {
-       console.log(`player:paper computer:rock`);
+        cGesture(cRock);
        UserScore++;
        startingCoins++;
-       console.log('Win');
+       
     } else {
         // computer choose gunting / player choose paper
-        console.log(`player:paper computer:scissor`);
+        cGesture(cScissor);
         ComputerScore++;
         startingCoins--;
-        console.log('Loose');
+        
     }
 
     userScore.textContent = UserScore;
@@ -129,52 +178,39 @@ function paperChoice(){
     coin.textContent = startingCoins;
 
     if (startingCoins === 0){
-        alert('Game Over');
         UserScore = 0;
         ComputerScore = 0;
         userScore.textContent = UserScore;
         computerScore.textContent = ComputerScore;
     }
-    // createLocalStorage();
+    createLocalStorage();
 }
 
 // Function for Scissors = 3;
 function scissorChoice(){
-    // console.log('paper');
-    // Create Element for image
-    const img = document.createElement('img'); //create img element 
-    img.setAttribute('src', 'images/scissor-left.png'); //add src attribute to img tag
-    img.setAttribute('height', '300'); // add height attribute to img tag
-    document.querySelector('.userPick').appendChild(img);
-
-    // Replace text on strong tag
-    const strong = document.createTextNode('You made a pick!').textContent;
-    document.querySelector('.strong').innerHTML = strong;
-
-    // Replace text on takePick
-    const waitPick = document.createTextNode('Wait for the opponent').textContent;
-    document.querySelector('.pickMsg').style.fontSize = '40px';
-    document.querySelector('.pickMsg').innerHTML = waitPick;
+    
+    // uGestures Append
+    uGesture(uScissor);
+    appendMsg();
 
     playerChoice = 3;
     computerChoice = Math.floor(Math.random() * 3 + 1);
     
     if(playerChoice === computerChoice) {
-        console.log(`player:scissor computer:scissor`);
-        console.log('draw');
+        cGesture(cScissor);
     }
     // computer choose rock / player choose paper
      else if (playerChoice == 3 && computerChoice == 1) {
-       console.log(`player:scissor computer:paper`);
+        cGesture(cRock);
         UserScore++
         startingCoins++
-       console.log('Win');
+        
     } else {
         // computer choose gunting / player choose paper
-        console.log(`player:scissor computer:rock`);
+        cGesture(cScissor);
         ComputerScore++;
         startingCoins--
-        console.log('Loose');
+        
     }
 
     userScore.textContent = UserScore;
@@ -182,27 +218,12 @@ function scissorChoice(){
     coin.textContent = startingCoins;
 
     if (startingCoins === 0){
-        alert('Game Over');
+        alert('game ove');
         UserScore = 0;
         ComputerScore = 0;
         userScore.textContent = UserScore;
         computerScore.textContent = ComputerScore;
     }
-    // createLocalStorage();
+    createLocalStorage();
 }
 
-function drawAlert(){
-    setTimeout(() => {
-        let draw;
-    draw += `
-    <div class="alert alert-dismissible alert-primary  m-5">
-        <p style="font-size: 50px;"> Oh snap, Its Draw! </p>
-        <p style="font-size: 20px;"> Current Score: <span class="badge badge-pill badge-light">10</span></p>
-        <button type="button" class="btn btn-outline-primary">Reset</button>
-        <button type="button" class="btn btn-info">Continue</button>
-    </div>
-    `;
-    }, 3000);
-    
-    
-}
